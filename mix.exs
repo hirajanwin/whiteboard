8196b1c8612c7d160b7e3efaca1eeab4ce415851
Zack Michener <z@zjm.me>
2020-04-15 19:43:25 -0700

    beef up elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Whiteboard.MixProject do
     [
       app: :whiteboard,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -40,6 +40,7 @@ defmodule Whiteboard.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
     ]
