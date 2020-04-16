9a23c284e85226c023b6387bbc87236df024a14c
Zack Michener <z@zjm.me>
2020-04-15 18:43:25 -0700

    add board channel

--- lib/whiteboard_web/router.ex
+++ lib/whiteboard_web/router.ex
@@ -17,6 +17,8 @@ defmodule WhiteboardWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+
+    resources "/boards", BoardController
   end
 
   # Other scopes may use custom stacks.
---
+++ test/whiteboard_web/channels/board_channel_test.exs
@@ -0,0 +1,26 @@
+defmodule WhiteboardWeb.BoardChannelTest do
+  use WhiteboardWeb.ChannelCase
+
+  setup do
+    {:ok, _, socket} =
+      socket(WhiteboardWeb.UserSocket, "user_id", %{some: :assign})
+      |> subscribe_and_join(WhiteboardWeb.BoardChannel, "board:lobby")
+
+    {:ok, socket: socket}
+  end
+
+  test "ping replies with status ok", %{socket: socket} do
+    ref = push socket, "ping", %{"hello" => "there"}
+    assert_reply ref, :ok, %{"hello" => "there"}
+  end
+
+  test "shout broadcasts to board:lobby", %{socket: socket} do
+    push socket, "shout", %{"hello" => "all"}
+    assert_broadcast "shout", %{"hello" => "all"}
+  end
+
+  test "broadcasts are pushed to the client", %{socket: socket} do
+    broadcast_from! socket, "broadcast", %{"some" => "data"}
+    assert_push "broadcast", %{"some" => "data"}
+  end
+end
